a085c2e8e2a640620dee7c470a3931e743c2e2ae
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-31 01:11:03 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/Views/FocusedEffectPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practica7.Models;
+using Xamarin.Forms;
+using Practica7.ViewModels;
+using Xamarin.Forms.Xaml;
+
+namespace Practica7.Views
+{
+    
+    public partial class FocusedEffectPage : ContentPage
+    {
+        public FocusedEffectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
