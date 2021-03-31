bc80e08e3ea981706a8f0abda18c21c22cc96810
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-31 00:46:27 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Practica7.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+//[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Practica7
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            InitializeComponent();
+            //Escribe aqui el nombre del View que deseas visualizar
+            MainPage = new SoftInputModeAdjustPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
