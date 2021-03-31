1a0e60deb2569eb8480110f4fcdc5fad5d11619a
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-31 00:59:24 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7.iOS/Services/DeviceOrientationIOS.cs
@@ -0,0 +1,21 @@
+﻿using Practica7.Services;
+using Xamarin.Forms;
+using DependencyServiceDemos.iOS;
+using UIKit;
+using Practica7.Models;
+
+[assembly: Dependency(typeof(DeviceOrientationService))]
+namespace DependencyServiceDemos.iOS
+{
+    public class DeviceOrientationService : IDeviceOrientationService
+    {
+        public DeviceOrientation GetOrientation()
+        {
+            UIInterfaceOrientation orientation = UIApplication.SharedApplication.StatusBarOrientation;
+
+            bool isPortrait = orientation == UIInterfaceOrientation.Portrait ||
+                orientation == UIInterfaceOrientation.PortraitUpsideDown;
+            return isPortrait ? DeviceOrientation.Portrait : DeviceOrientation.Landscape;
+        }
+    }
+}
