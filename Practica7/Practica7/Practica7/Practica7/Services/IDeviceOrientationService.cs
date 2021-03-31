002f57048008e8628a06a33f866a8a5c4ff036cb
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-31 01:04:10 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/Services/IDeviceOrientationService.cs
@@ -0,0 +1,14 @@
+﻿using Practica7.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Practica7.Services
+{
+   public interface IDeviceOrientationService
+    {
+        DeviceOrientation GetOrientation();
+
+    }
+}
