8d15942e951b8cd08e5640b13d0c33328f4ed6f4
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 13:37:04 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/Effects/FocusedEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Practica7.Effects
+{
+      public class FocusedEffect : RoutingEffect
+	{
+        public FocusedEffect() : base("Intec.FocusedEffect")
+        {
+        }
+
+
+    }
+    
+    
+}
