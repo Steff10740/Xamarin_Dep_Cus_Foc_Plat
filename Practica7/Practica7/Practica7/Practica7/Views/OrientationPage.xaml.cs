c4c7d7f4427c7603ee74fd64f27d90726a39aaaa
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 13:28:12 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/Views/OrientationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Practica7.Models;
+using Practica7.Services;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Practica7
+{
+    public partial class OrientationPage : ContentPage
+    {
+        public OrientationPage()
+        {
+            ///InitializeComponent();
+        }
+
+        void OnGetDeviceOrientationButtonClicked(object sender, EventArgs e)
+        {
+            DeviceOrientation orientation = DependencyService.Get<IDeviceOrientationService>().GetOrientation();
+           ///orientationLabel.Text = orientation.ToString();
+        }
+
+    }
+}
---
+++ Practica7/Practica7/Practica7/Practica7/Views/SoftInputModeAdjustPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Practica7.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SoftInputModeAdjustPage : ContentPage
+    {
+        public SoftInputModeAdjustPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnPanButtonClicked(object sender, EventArgs e)
+        {
+            App.Current.On<Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Pan);
+        }
+
+        void OnResizeButtonClicked(object sender, EventArgs e)
+        {
+            App.Current.On<Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
+        }
+    }
+}
