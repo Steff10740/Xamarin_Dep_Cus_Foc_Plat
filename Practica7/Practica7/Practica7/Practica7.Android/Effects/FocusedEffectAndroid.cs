3def297c476bdf9536f5f82c0e6123deaed44762
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 13:11:27 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7.Android/Effects/FocusedEffectAndroid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ResolutionGroupName("Intec")]
+[assembly: ExportEffect(typeof(Practica7.Droid.Services.FocusedEffectAndroid), nameof(Practica7.Droid.Services.FocusedEffectAndroid))]
+namespace Practica7.Droid.Services
+{
+    public class FocusedEffectAndroid : PlatformEffect
+    {
+        Android.Graphics.Color originalBackgroundColor = new Android.Graphics.Color(10, 20, 50, 20);
+        Android.Graphics.Color backgroundColor;
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                backgroundColor = Android.Graphics.Color.Blue;
+                Control.SetBackgroundColor(backgroundColor);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (((Android.Graphics.Drawables.ColorDrawable)Control.Background).Color == backgroundColor)
+                    {
+                        Control.SetBackgroundColor(originalBackgroundColor);
+                    }
+                    else
+                    {
+                        Control.SetBackgroundColor(backgroundColor);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+    }
+}
---
+++ Practica7/Practica7/Practica7/Practica7.Android/Effects/LabelShadowEffectAndroid.cs
@@ -0,0 +1,52 @@
+﻿using Practica7.Droid.Services;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Practica7.Effects.MyCompany.Forms.PlatformConfiguration.Android;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(LabelShadowEffectAndroid), "LabelShadowEffectAndroid")]
+namespace Practica7.Droid.Services
+{
+	public class LabelShadowEffectAndroid : PlatformEffect
+	{
+		protected override void OnAttached()
+		{
+			UpdateShadow();
+		}
+
+		protected override void OnDetached()
+		{
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			if (args.PropertyName == ShadowEffect.IsShadowedProperty.PropertyName)
+			{
+				UpdateShadow();
+			}
+		}
+
+		void UpdateShadow()
+		{
+			try
+			{
+				if (((Label)Element).OnThisPlatform().IsShadowed())
+				{
+					float radius = 5;
+					float distanceX = 5;
+					float distanceY = 5;
+					Android.Graphics.Color color = Android.Graphics.Color.Black;
+					(Control as Android.Widget.TextView).SetShadowLayer(radius, distanceX, distanceY, color);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+			}
+		}
+	}
+}
