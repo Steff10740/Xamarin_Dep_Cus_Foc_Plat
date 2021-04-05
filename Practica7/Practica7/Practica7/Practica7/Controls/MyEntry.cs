082fdae3e7bc10676d9c9753f7557b15f7a250b5
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 13:38:59 -0400

    Add files via upload

---
+++ Practica7/Practica7/Practica7/Practica7/Controls/MyEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Practica7.Controls
+{
+    public class MyEntry : Entry
+    {
+        
+    }
+}
